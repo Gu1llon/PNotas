4d1b70eae92297809833351026ddd134a09db66e
Gu1llon <g.cuevas852@gmail.com>
2017-05-11 14:19:16 -0300

    beta 0.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BurnOut!';
+  wenna = 'Felicidades no necesitas rendir exámen';
+  malaza = 'Lo lamento vas a exámen con nota: ';
+  noporce = 'La suma de todos los porcentajes debe ser igual a 100';
   //Variables de notas
-  nota1:number;
-  nota2:number;
-  nota3:number;
-  nota4:number;
-  nota5:number;
+  nota1:number=33;
+  nota2:number=25;
+  nota3:number=55;
+  nota4:number=65;
+  nota5:number=60;
   //Variables de porcentajes
-  porce1:number;
-  porce2:number;
-  porce3:number;
-  porce4:number;
-  porce5:number;
-  porce6:number;
+  porce1:number=20;
+  porce2:number=20;
+  porce3:number=20;
+  porce4:number=20;
+  porce5:number=20;
+  porce6:number=20;
+  porceok:number=100;
+  // tuNota:number=this.nota1*this.porce1/100+this.nota2*this.porce2/100+this.nota3*this.porce3/100+this.nota4*this.porce4/100+this.nota5*this.porce5/100;
+  //Variables de las funciones
+  notas:Array<any> = [];
+  porcentajes:Array<any> = [];
+  porceRestante:number = 100;
+  calculateScore(){
+    
+  }
 }
